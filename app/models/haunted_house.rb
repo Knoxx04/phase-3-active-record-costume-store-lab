a34641b4d06d57e0aca5a06c8842c0d2dde2b67c
Knoxx04 <onyiknoxx3@gmail.com>
2023-02-25 09:45:35 +0300

    halfway done

--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,1 +1,14 @@
-# Create your HauntedHouse class here
+class HauntedHouse < ActiveRecord::Migration[6.1]
+def change
+    create_table :haunted_houses do |t|
+        t.string :name
+        t.string :location
+        t.string :theme
+        t.float :price
+        t.boolean :family_friendly
+        t.date_time :opening_date
+        t.date_time :closing-date
+        t.text :description
+    end
+end
+end
